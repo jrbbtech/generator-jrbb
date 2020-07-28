a2959cccfca0545a1635c86292297d081521e23d
Robert Brewitz <hello@robertbrewitz.com>
2020-07-28 23:11:05 +0200

    ADDS tsamplify setup.

---
+++ generators/tsamplify/index.js
@@ -0,0 +1,61 @@
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+  writing() {
+    this.composeWith(require.resolve('../codestyle'));
+
+    this.fs.copyTpl(
+      this.templatePath('_gitignore'),
+      this.destinationPath('.gitignore')
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('tslint.json'),
+      this.destinationPath('tslint.json')
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('tsconfig.json'),
+      this.destinationPath('tsconfig.json')
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('jest.config.js'),
+      this.destinationPath('jest.config.js')
+    );
+
+    try {
+      const pkgConfig = require(this.destinationPath('package.json'));
+
+      pkgConfig.scripts = pkgConfig.scripts || {};
+      pkgConfig.scripts['build'] = 'tsc';
+      pkgConfig.scripts['test'] = 'jest';
+      pkgConfig.scripts['test:watch'] = 'jest --watch';
+      pkgConfig.scripts['lint'] = 'tslint -c tslint.json -p tsconfig.json';
+      pkgConfig.scripts['lint:fix'] =
+        'tslint -c tslint.json -p tsconfig.json --fix';
+
+      this.fs.writeJSON(this.destinationPath('package.json'), pkgConfig);
+    } catch (e) {
+      console.error(e);
+      console.info('Do you have a package.json?');
+    }
+  }
+
+  install() {
+    this.npmInstall(
+      [
+        '@types/jest',
+        'jest',
+        'ts-jest',
+        'tslint',
+        'tslint-config-prettier',
+        'tslint-plugin-prettier',
+        'typescript',
+      ],
+      {
+        'save-dev': true,
+      }
+    );
+  }
+};
